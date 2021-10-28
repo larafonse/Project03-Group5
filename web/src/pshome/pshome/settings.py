143570a126e7443ea2a9b052b675cf978aae0e07
Nicolas Lara Fonseca <53790517+larafonse@users.noreply.github.com>
2021-10-27 17:18:46 -0700

    project deployed

--- web/src/pshome/pshome/settings.py
+++ web/src/pshome/pshome/settings.py
@@ -18,14 +18,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
+# HEROKU CONFIG SECRET KEY : yiv4alVUkCb7YIwT6d7vc90ypSub3HB74dAggSGSjQez1ZjTLB
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '(d-p_)a)hmgnh089i%^hyvp8kjz!v)c_k7*+y#iemc!ex1c5j+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=1)) 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','project-share-g5.herokuapp.com']
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/src/pages/static/'
